14124e726419c7549898d42c97e362842a8ffc73
muhammadnehal610 <153855864+muhammadnehal610@users.noreply.github.com>
2024-11-01 21:23:50 +0500

    working

--- src/app/api/catagories/route.js
+++ src/app/api/catagories/route.js
@@ -1,15 +1,18 @@
 import { connectDB } from "@/lib/db/connectDB";
-import { CategoryModal } from "@/lib/modals/catagory";
+import { CategoryModal } from "@/lib/modals/category";
 
 export async function GET(request) {
   await connectDB();
-  const categories = await CategoryModal.find();
-  return Response.json(
-    {
+  const categories = await CategoryModal.find(
+    {},
+    "title description thumbnail"
+  );
+  return new Response(
+    JSON.stringify({
       msg: "Categories Fetched Successfully",
       categories,
-    },
-    { status: 200 }
+    }),
+    { status: 200, headers: { "Content-Type": "application/json" } }
   );
 }
 
@@ -19,15 +22,11 @@ export async function POST(request) {
   let newCategory = new CategoryModal(obj);
   await newCategory.save();
 
-  return Response.json(
-    {
-      msg: "Category Added Successfully ",
+  return new Response(
+    JSON.stringify({
+      msg: "Category Added Successfully",
       category: newCategory,
-    },
-    { status: 201 }
+    }),
+    { status: 201, headers: { "Content-Type": "application/json" } }
   );
 }
-
-export async function PUT(request) {}
-
-export async function DELETE(request) {}
